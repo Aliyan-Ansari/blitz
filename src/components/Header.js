282251af43eb71431eecc2ff598d29eb98941f92
UmerGillani36 <syedomershahgillani.com>
2023-03-31 02:31:01 -0700

    search and header slider issues solved

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,17 +16,16 @@ const Header = () => {
   };
   return (
     <div className={classes.header}>
-      <div></div>
       <div className={classes.actions}>
         <a className={`${classes.headerButton} ${classes.signIn}`}>
-          <BsPersonCircle style={{marginRight:10}}/> Sign In
+          <BsPersonCircle style={{ marginRight: 10 }} /> Sign In
         </a>
         {/* <a className={Object.assign(classes.headerButton, classes.window)}> */}
         <a
           className={`${classes.headerButton} ${classes.window}`}
           title="Windows 64-bit"
         >
-          <AiFillWindows style={{marginRight:10}}/> Download
+          <AiFillWindows style={{ marginRight: 10 }} /> Download
         </a>
       </div>
       <div style={{ margin: "5px 10px", padding: "5px", position: "relative" }}>
@@ -35,30 +34,32 @@ const Header = () => {
             position: "absolute",
             top: "50%",
             transform: "translateY(-50%)",
-            left: "14px",
+            left: "20px",
+            fontSize: 18,
+            marginRight: 10,
             color: isActive ? "hsl(222deg, 5%, 62%)" : "hsla(222deg, 5%, 62%)",
           }}
         />
         <input
           type="text"
           style={{
-            paddingLeft: "30px",
+            paddingLeft: "45px",
             borderRadius: "5px",
             backgroundColor: isActive
               ? "hsl(0, 0%, 100%)"
               : "hsl(222, 10%, 17%)",
             color: isActive ? "hsl(222deg, 5%, 62%)" : "hsla(222deg, 5%, 62%)",
-            width: "400px",
-            height: "40px",
+            width: "584px",
+            height: "56px",
             border: "none",
             outline: "none",
+            fontSize: 18,
           }}
           onFocus={handleFocus}
           onBlur={handleBlur}
           placeholder="Search profiles, champions, agents, legends, and more!"
         />
       </div>
- 
     </div>
   );
 };
