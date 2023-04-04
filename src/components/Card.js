f96ab2639d5bf7cb221c14a50e83d9ce4898024c
aliyan-ansari <aliyanansari321@gmail.com>
2023-04-04 16:49:04 +0500

    small fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import classes from "./Card.module.css";
 import { AiFillSetting } from "react-icons/ai";
+import { Tilt } from "react-tilt";
 import empty from "../Assets/Images/empty.jpeg";
 import {
   Modal,
@@ -11,25 +12,51 @@ import {
   ModalBody,
   ModalCloseButton,
   useDisclosure,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 const Card = (props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const defaultOptions = {
+    reverse: false, // reverse the tilt direction
+    max: 15, // max tilt rotation (degrees)
+    perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+    scale: 1.03, // 2 = 200%, 1.5 = 150%, etc..
+    speed: 3000, // Speed of the enter/exit transition
+    transition: true, // Set a transition on enter/exit.
+    axis: null, // What axis should be disabled. Can be X or Y.
+    reset: true, // If the tilt effect has to be reset on exit.
+    easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+  };
 
   return (
     <>
       <div key={props.key} className={classes.card}>
-        <div className={classes.imageContainer}>
-          <img src={props.url ? props.url : empty} className={classes.image} onClick={()=>{
-            onOpen();
-          }
-          }
-          />
-          <div className={classes.settingIcon}>
-            <AiFillSetting style={{ fontSize: 20, color: "#999ca3" }} />
+        <Tilt
+          options={defaultOptions}
+          style={{ height: "153px", width: "238px" }}
+        >
+          <div className={classes.imageContainer}>
+            <img
+              src={props.url ? props.url : empty}
+              className={classes.image}
+              onClick={() => {
+                onOpen();
+              }}
+            />
+            <div className={classes.settingIcon}>
+              <AiFillSetting style={{ fontSize: 20, color: "#999ca3" }} />
+            </div>
           </div>
-        </div>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
+        </Tilt>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            margin: "10px 0",
+            fontSize: "calc(0.25 * 4em)",
+            fontWeight: 700,
+          }}
+        >
           <p className={classes.title}>{props.title}</p>
         </div>
         <div className={classes.chipList}>
