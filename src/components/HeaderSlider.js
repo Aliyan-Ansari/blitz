f2187d3df9b20cb61e076ad87345aabfbb59eef3
aliyan-ansari <aliyanansari321@gmail.com>
2023-04-13 15:42:07 +0500

    header done

--- src/components/HeaderSlider.js
+++ src/components/HeaderSlider.js
@@ -4,10 +4,10 @@ import classes from './HeaderSlider.module.css';
 
 const defaultOptions = {
   reverse: false, // reverse the tilt direction
-  max: 15, // max tilt rotation (degrees)
+  max: 20, // max tilt rotation (degrees)
   perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-  scale: 1.03, // 2 = 200%, 1.5 = 150%, etc..
-  speed: 3000, // Speed of the enter/exit transition
+  scale: 1.04, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 4000, // Speed of the enter/exit transition
   transition: true, // Set a transition on enter/exit.
   axis: null, // What axis should be disabled. Can be X or Y.
   reset: true, // If the tilt effect has to be reset on exit.
@@ -63,7 +63,7 @@ const HeaderSlider = () => {
       {games.map((value) => (
         <Tilt
           options={defaultOptions}
-          className={`${classes.headerCard} ${classes[value.class]}`}
+          className={` ${classes.headerCard} ${classes[value.class]}`}
         >
           <img
             src={value.coverImage}
